6f1ca02022d4e493db73e650607e4f7a1aaae28e
Mark Johnson <mrk.jhsn@gmail.com>
2019-03-31 16:24:26 -0700

    Update Departments_Added_Removed.sql

--- Departments_Added_Removed.sql
+++ Departments_Added_Removed.sql
@@ -1,13 +1,11 @@
 -- year over year, how many departments get added/removed? is that indicative of ASU strategy?
 -- create table 'Department Changes', then populate that table with additions and removals for each year
 
-	
-	select * from [dbo].[ASU Employee Salary Data - 2012]
-
 	select 
-		xxx.Calendar_Year
-		,xxx.Department
-		,xxx.ADD_or_REM
+		ADD_REM_AllYears.Calendar_Year
+		,ADD_REM_AllYears.Department
+		,ADD_REM_AllYears.ADD_or_REM
+		,ADD_REM_AllYears.Total_Salary
 	into [ASU_Professor_Salary].[dbo].[ASU_Dept_Changes]
 	from 
 	(select   -- 2013
@@ -86,6 +84,83 @@ union all
 	where FY14.Department_Description is null
 	group by FY15.Calendar_Year, FY15.Department_Description
 	
+union all
+
+	select 
+		'2016' as Calendar_Year
+		,FY15.Department_Description as Department 
+		,'REM' as ADD_or_REM
+		,sum(FY15.Salary) as Total_Salary
+	from [dbo].[ASU Employee Salary Data - 2015] as FY15
+	full outer join [dbo].[ASU Employee Salary Data - 2016] as FY16
+		on FY16.[Department_Description] = FY15.Department_Description 
+	where FY16.Department_Description is null
+	group by FY15.Department_Description
+
+union all
 	
+	select 
+		FY16.Calendar_Year
+		,FY16.Department_Description as Department 
+		,'ADD' as ADD_or_REM
+		,sum(FY16.Salary) as Total_Salary
+	from [dbo].[ASU Employee Salary Data - 2015] as FY15
+	full outer join [dbo].[ASU Employee Salary Data - 2016] as FY16
+		on FY16.[Department_Description] = FY15.Department_Description 
+	where FY15.Department_Description is null
+	group by FY16.Calendar_Year, FY16.Department_Description
 	
-	) As ADD_REM2013
+union all
+
+	select 
+		'2017' as Calendar_Year
+		,FY16.Department_Description as Department 
+		,'REM' as ADD_or_REM
+		,sum(FY16.Salary) as Total_Salary
+	from [dbo].[ASU Employee Salary Data - 2016] as FY16
+	full outer join [dbo].[ASU Employee Salary Data - 2017] as FY17
+		on FY17.[Department_Description] = FY16.Department_Description 
+	where FY17.Department_Description is null
+	group by FY16.Department_Description
+
+union all
+	
+	select 
+		FY17.Calendar_Year
+		,FY17.Department_Description as Department 
+		,'ADD' as ADD_or_REM
+		,sum(FY17.Salary) as Total_Salary
+	from [dbo].[ASU Employee Salary Data - 2016] as FY16
+	full outer join [dbo].[ASU Employee Salary Data - 2017] as FY17
+		on FY17.[Department_Description] = FY16.Department_Description 
+	where FY16.Department_Description is null
+	group by FY17.Calendar_Year, FY17.Department_Description
+
+union all
+
+	select 
+		'2018' as Calendar_Year
+		,FY17.Department_Description as Department 
+		,'REM' as ADD_or_REM
+		,sum(FY17.Salary) as Total_Salary
+	from [dbo].[ASU Employee Salary Data - 2017] as FY17
+	full outer join [dbo].[ASU Employee Salary Data - 2018] as FY18
+		on FY18.[Department_Description] = FY17.Department_Description 
+	where FY18.Department_Description is null
+	group by FY17.Department_Description
+
+union all
+	
+	select 
+		FY18.Calendar_Year
+		,FY18.Department_Description as Department 
+		,'ADD' as ADD_or_REM
+		,sum(FY18.Salary) as Total_Salary
+	from [dbo].[ASU Employee Salary Data - 2017] as FY17
+	full outer join [dbo].[ASU Employee Salary Data - 2018] as FY18
+		on FY18.[Department_Description] = FY17.Department_Description 
+	where FY17.Department_Description is null
+	group by FY18.Calendar_Year, FY18.Department_Description
+
+	
+	) As ADD_REM_AllYears
